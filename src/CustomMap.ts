f5cd20c4a159ce385b2e29cc2a9b247e25b7a735
Muskaan <muskaansingla2011@gmail.com>
2022-06-07 23:37:38 +0530

    added a basic ts app

---
+++ src/CustomMap.ts
@@ -0,0 +1,42 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor() {
+    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(entity: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: entity.location.lat,
+        lng: entity.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: entity.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
